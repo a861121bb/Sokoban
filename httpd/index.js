0aaf936c005709d000f0791e8c85f29f97a7d22d
a861121bb <a861121bbb@gmail.com>
2017-11-03 15:26:22 +0800

    ...

--- httpd/index.js
+++ httpd/index.js
@@ -1,24 +1,24 @@
- 'use strict';
+'use strict';
 
 let http = require('http');
 
 http.createServer((request, response) => {
-let fs = require('fs');
-let postData = ''; // POST 資料
+ let fs = require('fs');
+ let postData = ''; // POST 資料
 
 
- // 利⽤ 'data' event 消耗掉 data chunk;
+// 利⽤ 'data' event 消耗掉 data chunk;
  // 'end' event 才會被 fired
- request.on('data', (chunk) =>
- postData += chunk;
+ request.on('data', (chunk) => {
+postData += chunk;
 
-console.log(
- ' 接收的 POST data ⽚段 k: [' + chunk + '].'
+ console.log(
+' 接收的 POST data ⽚段 k: [' + chunk + '].'
  );
-});
+ });
 
-request.on('end', () => {
-switch (request.url) {
+ request.on('end', () => {
+ switch (request.url) {
  case '/':
  fs.readFile('../htdocs/index.html', (err, data) => {
  if (err) {
@@ -32,7 +32,7 @@ switch (request.url) {
  response.write(data);
  response.end();
  }
- });
+});
 
  break;
 

62b07c2d09268d712e8621993aa1e6ade225e833
a861121bb <a861121bbb@gmail.com>
2017-12-08 14:18:50 +0800

    ...

--- httpd/index.js
+++ httpd/index.js
@@ -1,68 +1,66 @@
-/**
- *  @file       index.js
- *  @brief      The entry file of Sokoban.
- *  @author     Yiwei Chiao (ywchiao@gmail.com)
- *  @date       11/17/2017 created.
- *  @date       12/07/2017 last modified.
- *  @version    0.1.0
- *  @copyright  MIT, (C) 2017 Yiwei Chiao
- *  @details
- *
- *  The entry file of Sokoban.
- */
 'use strict';
 
-/**
- * sokoban 程式進入點
+let http = require('http');
+
+const routingTable = require('./config.json');
+
+ /**
+ * 利⽤ http.ServerResponse 物件回傳檔案內容
  *
- * @callback
- * @param 'load' : DOM 事件名
+ * @name serve
+ * @function
+ * @param response - http.ServerResponse 物件
+ * @param fname - 要回傳的檔案名
+ * @param datatype - 回傳檔案內容的 Mine-Type
  * @returns {undefined}
  */
-window.addEventListener('load', () => {
-  console.log("Sokoban.js loaded");
+ let serve = (response, fname, datatype) => {
+ let fs = require('fs');
 
-  let gameTitle = document.createElement('span');
-  gameTitle.textContent = 'Soboban';
+ fs.readFile(fname, (err, data) => {
+ if (err) {
+console.log(' 檔案讀取錯誤');
+ }
+ else {
+ response.writeHead(200, {
+ 'Content-Type': datatype
+ });
 
-  let gameHeader = document.createElement('header');
-  gameHeader.className = 'card_header';
-
-  gameHeader.appendChild(gameTitle);
-
-  let sokobanCanvas = document.createElement('canvas');
-
-  let sokobanBoard = document.createElement('div');
-  sokobanBoard.style.gridArea = '1 / 2 / 4 / 5';
-
-  sokobanBoard.appendChild(sokobanCanvas);
-
-  let gameBoard = document.createElement('article');
-  gameBoard.className = 'card_content';
-
-  gameBoard.appendChild(sokobanBoard);
-
-  let gameDesktop = document.createElement('section');
-  gameDesktop.className = 'card';
-
-  gameDesktop.appendChild(gameHeader);
-  gameDesktop.appendChild(gameBoard);
-
-  let desktop = document.querySelector('.site_body')
-  desktop.appendChild(gameDesktop);
-
-  /**
-   * 滑鼠游標移動追踪
-   *
-   * @callback
-   * @param 'mousemove' : DOM 事件名
-   * @param e : DOM event 物件
-   * @returns {undefined}
-   */
-  desktop.addEventListener('mousemove', (e) => {
-    document.getElementById('cursor_x').textContent = e.clientX;
-    document.getElementById('cursor_y').textContent = e.clientY;
-  });
+response.write(data);
+response.end();
+ }
 });
+ };
 
-// index.js
+ http.createServer((request, response) => {
+ let fs = require('fs');
+
+ let postData = '';
+ // 利⽤ 'data' event 消耗掉 data chunk;
+ // 'end' event 才會被 fired
+request.on('data', (chunk) => {
+ postData += chunk;
+
+console.log(
+' 接收的 POST data ⽚段 k: [' + chunk + '].'
+ );
+ });
+
+request.on('end', () => {
+ if (request.url in routingTable) {
+ let obj = routingTable[request.url];
+
+ serve(response, obj.url, obj.type);
+ }
+ else {
+ console.log(' 未定義的存取: ' + request.url);
+
+ response.end();
+ }
+ });
+ }).listen(8088);
+
+ // log message to Console
+ console.log(' 伺服器啓動，連線 url: http://127.0.0.1:8088/');
+
+ // index.js
